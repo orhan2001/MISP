8016905b39d31971feeaf5c18184243da25349b0
orhan gadzhibalaev <orhan4182@outlook.com>
2021-11-16 20:00:59 +0300

    1 задание

---
+++ Скобка 1 задание/main.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<stack>
+#include<string>
+
+void Error(int ii) {
+	std::cout << "Error in " << ii << " index!\n";
+}
+
+int main() {
+	std::stack<char> st;
+
+	std::cout << "Give me a string\n";
+	char str[30];
+	std::cin >> str;
+
+	char ch;
+	int i = 0;
+	int count = 0;
+
+	for (i = 0;i < sizeof(str);i++) {
+		ch = str[i];
+		if ((i == 0) && ((ch != '(') && (ch != '{') && (ch != '['))) {
+			Error(i);
+			continue;
+		}
+		else {
+			if ((ch == '(') || (ch == '{') || (ch == '[')) {
+				st.push(ch);
+				count++;
+				continue;
+			}
+			else {
+				if ((ch == ')') && (st.top() != '(')) {
+					Error(i);
+				}
+				if ((ch == ']') && (st.top() != '[')) {
+					Error(i);
+				}
+				if ((ch == '}') && (st.top() != '{')) {
+					Error(i);
+				}
+			}
+		}
+	}
+}
